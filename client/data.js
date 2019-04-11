931b16b95c60eb8bffcc24b679780da98480fe52
John Winiata Tapsell <johntapsell@windowslive.com>
2019-04-11 18:17:56 +1200

    fix squirtle 3

---
+++ client/data.js
@@ -0,0 +1,15 @@
+
+export default [
+  { id: 1, value: '/images/pikachu.png', isVisible: false, info: 'pikachu' },
+  { id: 2, value: '/images/squirtle.png', isVisible: false, info: 'squirtle' },
+  { id: 3, value: '/images/charmander.png', isVisible: false, info: 'charmander' },
+  { id: 4, value: '/images/bulbasaur.png', isVisible: false, info: 'bulbasaur' },
+  { id: 5, value: '/images/charmander.png', isVisible: false, info: 'charmander' },
+  { id: 6, value: '/images/bulbasaur.png', isVisible: false, info: 'bulbasaur' },
+  { id: 7, value: '/images/jigglypuff.png', isVisible: false, info: 'jigglypuff' },
+  { id: 8, value: '/images/squirtle.png', isVisible: false, info: 'squirtle' },
+  { id: 9, value: '/images/jigglypuff.png', isVisible: false, info: 'jigglypuff' },
+  { id: 10, value: '/images/meowth.png', isVisible: false, info: 'meowth' },
+  { id: 11, value: '/images/pikachu.png', isVisible: false, info: 'pikachu' },
+  { id: 12, value: '/images/meowth.png', isVisible: false, info: 'meowth' }
+]
