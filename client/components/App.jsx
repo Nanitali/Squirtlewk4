0452f681e8a66d63862e27c800aaefe8d6894dad
Eve Te Uru Henare <evehenare1@gmail.com>
2019-04-12 11:06:46 +1200

    add Route to board in App.js

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
+
 import Ball from './Ball'
 import Board from './Board'
+import Sound from './Sound'
 
 const App = () => {
   return (
     <React.Fragment>
       <Router exact path="/" component={Ball} />
       <Router exact path="/board" component={Board} />
+      <Router exact path="/sound" component={Sound} />
     </React.Fragment>
 
   )
