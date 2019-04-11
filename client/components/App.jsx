f89c45ec4ff7cda839d9980bd911582bf31f21a3
John Winiata Tapsell <johntapsell@windowslive.com>
2019-04-12 10:55:00 +1200

    Add ball and routes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
+import Ball from './Ball'
+import Board from './Board'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <React.Fragment>
+      <Router exact path="/" component={Ball} />
+      <Router exact path="/board" component={Board} />
+    </React.Fragment>
+
   )
 }
 
 export default App
-
---
+++ client/components/Ball.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Ball = () => {
+  return (
+    <React.Fragment>
+      <h1>Click to enter!</h1>
+      <img src="images/pokeball.png" className='ballSpin' ></img>
+
+    </React.Fragment>
+  )
+}
+
+export default Ball
