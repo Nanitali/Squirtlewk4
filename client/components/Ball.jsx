a0594545a7171d402fe5b014da589ea8c2313ef8
Eve Te Uru Henare <evehenare1@gmail.com>
2019-04-12 12:11:54 +1200

    ignore

--- client/components/Ball.jsx
+++ client/components/Ball.jsx
@@ -1,19 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-<<<<<<< HEAD
-const Ball = () => {
-  return (
-    <React.Fragment>
-      <h1>Click to enter!</h1>
-      {/* <img src="images/pokeball.png" className='ballSpin' onClick={<Link to='/board'/>}> </img> */}
-    </React.Fragment>
-
-  )
-=======
 
 class Ball extends React.Component {
   onClick = () => {
-    this.props.history.push("/board")
+    this.props.history.push('/board.')
   }
   render () {
 
@@ -25,7 +15,6 @@ class Ball extends React.Component {
 
     )
   }
->>>>>>> 7f6f289b05ed790513100c6f4b003fc3998d1f43
 }
 
 export default Ball
